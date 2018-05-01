70e49898b5dd0223b0f6ef6f4429784d57954174
Ali Bayati <bayati.ali@icloud.com>
2018-05-01 06:24:48 -0500

    incomplete code fixing the last method

---
+++ funWithFunction.py
@@ -0,0 +1,20 @@
+# def oddEve(num):
+#     if num %2!=0:
+#         return "an odd number"
+#     elif num %2==0:
+#         return "an even number"
+
+# for i in range(1,10):
+#     print "number is", i , "this is a",oddEve(i)
+a = [2,4,10,16]
+
+def multiply(liA,num):
+    liB=[i*5 for i in liA]
+    return liB
+
+print(multiply(a,5))
+
+
+def layerdMultip(fx)
+    lyMul=multiply(fx)
+    
